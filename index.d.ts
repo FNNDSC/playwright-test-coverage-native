df9bfc4be7fb64ec33218a85e4087b5431b88686
Jennings Zhang <jenni_zh@protonmail.com>
2024-01-24 19:31:32 -0500

    Add test to index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import type { PlaywrightTestConfig as BasePlaywrightTestConfig } from "@playwright/test";
+import type { PlaywrightTestConfig as BasePlaywrightTestConfig, test as originalTest } from "@playwright/test";
 
 export type PlaywrightTestConfig<T = {}, W = {}> = Omit<BasePlaywrightTestConfig<T, W>, "use"> & {
   use?: BasePlaywrightTestConfig<T, W>["use"] & {
@@ -13,5 +13,7 @@ export function defineConfig<T, W>(config: PlaywrightTestConfig<T, W>): Playwrig
 export function defineConfig(config: PlaywrightTestConfig, ...configs: PlaywrightTestConfig[]): PlaywrightTestConfig;
 export function defineConfig<T>(config: PlaywrightTestConfig<T>, ...configs: PlaywrightTestConfig[]): PlaywrightTestConfig<T>;
 export function defineConfig<T, W>(config: PlaywrightTestConfig<T, W>, ...configs: PlaywrightTestConfig[]): PlaywrightTestConfig<T, W>;
+
+export const test: typeof originalTest;
 
 export { expect, devices } from "@playwright/test";
